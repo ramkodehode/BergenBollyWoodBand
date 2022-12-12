3561017e0b8cf8d3013668b1a9967a9afb7ea81e
ramkodehode <ram.kodehode@gmail.com>
2022-12-12 09:02:14 +0100

    Test file

---
+++ test.js
@@ -0,0 +1,58 @@
+<div className="App">
+  <header className="App-header">
+    <h1>BergenBollywoodBand</h1>
+    <h2> Song list </h2>
+
+    <ol>
+      <li>Jahn Pechan Ho, Rock and Roll:</li>
+      <a target={'blank'} href={'https://www.youtube.com/watch?v=nOAsaehfPrk'}>
+        https://www.youtube.com/watch?v=nOAsaehfPrk
+      </a>
+      <br />
+      <br />
+      <li>Teri Bindiya Re, 7-beats:</li>
+      <h4>Orignal: </h4>
+
+      <a target={'blank'} href={'https://youtu.be/ulUZcdwTVaA'}>
+        https://youtu.be/ulUZcdwTVaA
+      </a>
+      <h4>Alternative: </h4>
+
+      <a target={'blank'} href={'https://youtu.be/_kqdWOWlbA4'}>
+        https://youtu.be/_kqdWOWlbA4
+      </a>
+      <br />
+      <br />
+      <li>Kya Yehi Pyar Hai, Ballad + Fast:</li>
+      <h4>Orignal: </h4>
+
+      <a target={'blank'} href={'https://youtu.be/9wI3UXRYW-s'}>
+        https://youtu.be/9wI3UXRYW-s
+      </a>
+      <h4>Alternative 1: </h4>
+
+      <a target={'blank'} href={'https://youtu.be/0vgh5lBo2Ek'}>
+        https://youtu.be/0vgh5lBo2Ek
+      </a>
+      <h4>Alternative 2: </h4>
+
+      <a target={'blank'} href={' https://youtu.be/YxgbjHYRxbQ'}>
+         https://youtu.be/YxgbjHYRxbQ
+      </a>
+
+      <br />
+      <br />
+      <li>Dil Se, medley:</li>
+      <h4>Orignal: </h4>
+
+      <a target={'blank'} href={'https://youtu.be/8ToEwbzM-0c'}>
+        https://youtu.be/8ToEwbzM-0c
+      </a>
+      <h4>Alternative: </h4>
+
+      <a target={'blank'} href={'https://youtu.be/8ToEwbzM-0c'}>
+        https://youtu.be/8ToEwbzM-0c
+      </a>
+    </ol>
+  </header>
+</div>;
